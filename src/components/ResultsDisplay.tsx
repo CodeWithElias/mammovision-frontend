e4b619724efcaa5b732092ad91a0e72f0e5ed2ab
smnunez404 <smnunez404@gmail.com>
2024-11-27 07:36:57 -0400

    COrreccion bugs - primer intento

--- src/components/ResultsDisplay.tsx
+++ src/components/ResultsDisplay.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { FiZoomIn, FiZoomOut, FiRotateCcw, FiDownload, FiPrinter } from 'react-icons/fi';
 import { PredictionResult } from '@/types/types';
 
 interface ResultsDisplayProps {
