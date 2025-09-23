c2aee5538e91e34ef6fa3cf441970565dd0f3c94
CodeWithElias <eliaspumaclaure48@gmail.com>
2025-09-22 22:36:44 -0400

    backend agregado

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { PredictionResult } from '@/types/types';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://192.168.1.41:10000';
+// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://mammovision-backend-nxzk.onrender.com';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://mammovision-backend-leub.onrender.com';
 //const API_URL = 'http://192.168.1.41:10000';
 export const api = axios.create({
   baseURL: API_URL,
@@ -17,8 +18,17 @@ export const uploadImage = async (file: File): Promise<PredictionResult> => {
   try {
     const response = await api.post<PredictionResult>('/predict', formData);
     return response.data;
-  } catch (error) {
+  } catch (error: any) {
     console.error('Error uploading image:', error);
-    throw new Error('Error al procesar la imagen');
+    if (error.response) {
+      // El backend respondió con un código de error
+      throw new Error('Error del servidor: ' + error.response.status + ' - ' + error.response.statusText);
+    } else if (error.request) {
+      // No se recibió respuesta del backend
+      throw new Error('No se recibió respuesta del servidor. Por favor, intenta más tarde.');
+    } else {
+      // Otro error
+      throw new Error('Error al procesar la imagen');
+    }
   }
 };
